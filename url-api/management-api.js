dfc99e50b003bb604ae5ae82b17c17d14bc58e5e
Andy Duncan <andy@adjectivecolournoun.com>
2019-03-18 22:01:55 +0000

    Bootstrap

--- url-api/management-api.js
+++ url-api/management-api.js
@@ -8,13 +8,35 @@ const handlebars = require('handlebars');
 
 const waitFor = require('p-wait-for');
 
-const linkingFormTemplate = `<html xmlns="http://www.w3.org/1999/html">
+const linkingFormTemplate = `<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>to-link a page</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
+          integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+</head>
 <body>
-<form action="/shorten" method="post" id="shortenForm">
-<label for="url">Enter a URL to shorten</label>
-<input type="url" name="url" placeholder="http://www.example.com" pattern="https?://.+" required />
-<input type="submit">Shorten</input>
-</form>
+<div class="container">
+    <div class="row">
+        <h1 class="site-title">to-link</h1>
+    </div>
+    <div class="row">
+        <form action="/shorten" method="post" id="shortenForm">
+            <div class="form-group">
+                <label for="url">Page to link</label>
+                <input type="url" name="url" class="form-control" placeholder="http://www.example.com"
+                       pattern="https?://.+" required/>
+            </div>
+            <input type="submit" class="btn btn-default" value="Shorten"/>
+        </form>
+    </div>
+</div>
 </body>
 </html>`;
 
