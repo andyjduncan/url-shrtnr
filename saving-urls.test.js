1e20664927004ad6eb4c2b3e0a81f41a3a7b9649
Andy Duncan <andy@adjectivecolournoun.com>
2019-06-12 16:40:41 +0100

    Make ids as short as possible

--- saving-urls.test.js
+++ saving-urls.test.js
@@ -6,6 +6,8 @@ const saveUrls = require('./save-urls');
 
 const randString = () => Math.random().toString(36).substr(2, 5);
 
+const randNumber = () => Math.ceil(Math.random() * 5);
+
 const tableName = randString();
 
 beforeEach(() => {
@@ -15,9 +17,11 @@ beforeEach(() => {
 
 describe('generating id', () => {
     it('generates a new id', async () => {
-        const response = await saveUrls.generateId();
+        const retries = randNumber();
 
-        expect(response.shortId).toMatch(/^[-A-Za-z0-9_]{10}$/);
+        const response = await saveUrls.generateId({retries});
+
+        expect(response.shortId).toMatch(new RegExp(`^[-A-Za-z0-9_]{${retries + 2}}$`));
     });
 
     it('passes through the input url', async () => {
